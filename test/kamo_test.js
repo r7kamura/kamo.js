5aeeaa050da1c9a52c7a41bdbf8e7625fe1b1d1a
Ryo Nakamura <r7kamura@gmail.com>
2014-08-20 11:40:52 +0900

    Refactor: use sinon as subscription

--- test/kamo_test.js
+++ test/kamo_test.js
@@ -3,77 +3,66 @@ var assert = require('assert');
 var sinon = require('sinon');
 
 describe('kamo.Stream', function () {
-  beforeEach(function () {
-    this.stream = new kamo.Stream();
-  });
-
   describe('.fromEventHandlerSetter', function () {
-    it('creates a new Stream from an object and its setter', function (done) {
+    it('creates a new Stream from an object and its setter', function () {
+      var spy = sinon.spy();
       var object = {};
-      kamo.Stream.fromEventHandlerSetter(object, 'eventHandlerName').subscribe(function (message) {
-        done();
-      });
-      object.eventHandlerName('message');
+      kamo.Stream.fromEventHandlerSetter(object, 'onKeyup').subscribe(spy);
+      object.onKeyup('dummy key event');
+      assert(spy.called);
     });
   });
 
   describe('.fromEventHandlerFunction', function () {
-    it('creates a new Stream from an object, its function name, and optional arguments', function (done) {
+    it('creates a new Stream from an object, its function name, and optional arguments', function () {
+      var spy = sinon.spy();
       var object = {
-        eventHandlerName: function (publisher, argument1, argument2) {
-          this.publisher = publisher;
+        onClick: function (click, argument1, argument2) {
+          this.click = click;
           this.argument1 = 'argument1';
           this.argument2 = 'argument2';
         }
       };
       kamo.Stream.fromEventHandlerFunction(
         object,
-        'eventHandlerName',
+        'onClick',
         'argument1',
         'argument2'
-      ).subscribe(function (message) {
-        assert.equal(message, 'message');
-        done();
-      });
-      assert.equal(object.argument1, 'argument1');
-      assert.equal(object.argument2, 'argument2');
-      object.publisher('message');
+      ).subscribe(spy);
+      object.click();
+      assert(spy.called);
     });
   });
 
   describe('#publish', function () {
-    it('publishes a message to its subscribers in the order defined', function (done) {
-      var index = 0;
-      this.stream.subscribe(function (message) {
-        assert.equal(index, 0);
-        index++;
-      });
-      this.stream.subscribe(function (message) {
-        assert.equal(index, 1);
-        index++;
-        done();
-      });
-      this.stream.publish('message');
+    it('publishes a message to its subscribers in the order defined', function () {
+      var spy = sinon.spy();
+      var stream = new kamo.Stream();
+      stream.subscribe(spy);
+      stream.subscribe(spy);
+      stream.publish();
+      assert(spy.calledTwice);
     });
   });
 
   describe('#subscribe', function () {
-    it('subscribes a callback called with the published message', function (done) {
-      var publishedMessage = 'message';
-      this.stream.subscribe(function (message) {
-        assert.equal(message, publishedMessage);
-        done();
-      })
-      this.stream.publish(publishedMessage);
+    it('subscribes a callback called with the published message', function () {
+      var spy = sinon.spy();
+      var stream = new kamo.Stream();
+      var message = 'message';
+      stream.subscribe(spy);
+      stream.publish(message);
+      assert(spy.calledWith(message));
     });
   });
 
   describe('#merge', function () {
     it('creates a new Stream by merging 2 Stream', function () {
       var spy = sinon.spy();
+      var stream = new kamo.Stream();
       var anotherStream = new kamo.Stream();
-      this.stream.merge(anotherStream).subscribe(spy);
-      this.stream.publish();
+      stream.merge(anotherStream).subscribe(spy);
+      stream.publish();
       anotherStream.publish();
       assert(spy.calledTwice);
     });
