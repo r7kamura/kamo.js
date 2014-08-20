16394855e979a78b8d3f3705439005714dd35bd3
Ryo Nakamura <r7kamura@gmail.com>
2014-08-20 10:57:47 +0900

    Add unit tests for the class methods of Stream

--- test/kamo_test.js
+++ test/kamo_test.js
@@ -6,6 +6,40 @@ describe('kamo.Stream', function () {
     this.stream = new kamo.Stream();
   });
 
+  describe('.fromEventHandlerSetter', function () {
+    it('creates a new Stream from an object and its setter', function (done) {
+      var object = {};
+      kamo.Stream.fromEventHandlerSetter(object, 'eventHandlerName').subscribe(function (message) {
+        done();
+      });
+      object.eventHandlerName('message');
+    });
+  });
+
+  describe('.fromEventHandlerFunction', function () {
+    it('creates a new Stream from an object, its function name, and optional arguments', function (done) {
+      var object = {
+        eventHandlerName: function (publisher, argument1, argument2) {
+          this.publisher = publisher;
+          this.argument1 = 'argument1';
+          this.argument2 = 'argument2';
+        }
+      };
+      kamo.Stream.fromEventHandlerFunction(
+        object,
+        'eventHandlerName',
+        'argument1',
+        'argument2'
+      ).subscribe(function (message) {
+        assert.equal(message, 'message');
+        done();
+      });
+      assert.equal(object.argument1, 'argument1');
+      assert.equal(object.argument2, 'argument2');
+      object.publisher('message');
+    });
+  });
+
   describe('#publish', function () {
     it('publishes a message to its subscribers in the order defined', function (done) {
       var index = 0;
@@ -18,7 +52,7 @@ describe('kamo.Stream', function () {
         index++;
         done();
       });
-      this.stream.publish();
+      this.stream.publish('message');
     });
   });
 
